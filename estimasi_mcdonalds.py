aef974f9bdc95a6b2c6ad7f1ca6e7042121cac95
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-10-26 15:33:43 +0700

    estimasi_mcdonalds.py
    mencoba menambahkan inputan

--- estimasi_mcdonalds.py
+++ estimasi_mcdonalds.py
@@ -8,11 +8,12 @@ Cholesterols = st.number_input('Input Kolesterol (mg)')
 Total_carbohydrate = st.number_input('Input Total Karbohidrat (g)')
 Total_Sugars = st.number_input('Input Jumlah Gula (g)')
 Protein = st.number_input('Input Jumlah Protein (g)')
+lemak = st.number_input('Input Jumlah Lemak')
 
 predict = ''
 
 if st.button('Estimasi Energi'):
     predict = model.predict(
-        [[Cholesterols, Total_carbohydrate, Total_Sugars, Protein]]
+        [[Cholesterols, Total_carbohydrate, Total_Sugars, Protein, lemak]]
         )
     st.write ('Estimasi Jumlah Energi di setiap ukuran makanan McDonalds : ', predict)
