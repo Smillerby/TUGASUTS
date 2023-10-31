c13d441344984c5bf2ee12fe0d9f6bbaccee69f9
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-10-31 22:28:27 +0700

    Update estimasi_mcdonalds.py

--- estimasi_mcdonalds.py
+++ estimasi_mcdonalds.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 model = pickle.load(open('estimasi_mcdonalds.sav', 'rb'))
 
-st.title('Estimasi Jumlah Lemak Di Menu McDonalds')
+st.title('Estimasi Jumlah Lemak Padat Di Menu McDonalds')
 Cholesterols = st.number_input('Input Kolesterol (mg)')
 Total_carbohydrate = st.number_input('Input Total Karbohidrat (g)')
 Total_Sugars = st.number_input('Input Jumlah Gula (g)')
