de1d297ef3055e3cbfb2776e04491381f8e685b2
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-10-31 21:48:21 +0700

    Update estimasi_mcdonalds.py

--- estimasi_mcdonalds.py
+++ estimasi_mcdonalds.py
@@ -9,7 +9,7 @@ Total_carbohydrate = st.number_input('Input Total Karbohidrat (g)')
 Total_Sugars = st.number_input('Input Jumlah Gula (g)')
 Protein = st.number_input('Input Jumlah Protein (g)')
 Trans_fat = st.number_input('Input Jumlah Lemak(Lemak Jenuh)')
-Sat_Fat = st.number_input('Input Total Sat Fat(Lemak Padat)')
+Energy = st.number_input('Input Total Energy')
 Added_Sugars = st.number_input('Input Jumlah Gula Yang Di Tambahkan')
 Total_fat = st.number_input('Input Jumlah Total Lemak')
 
@@ -18,6 +18,6 @@ predict = ''
 
 if st.button('Estimasi Energi'):
     predict = model.predict(
-        [[Cholesterols, Total_carbohydrate, Total_Sugars, Protein, Trans_fat, Added_Sugars, Total_fat, Sat_Fat]]
+        [[Cholesterols, Total_carbohydrate, Total_Sugars, Protein, Trans_fat, Added_Sugars, Total_fat, Energy]]
         )
     st.write ('Estimasi Jumlah Nutrisi di setiap ukuran makanan McDonalds : ', predict)
